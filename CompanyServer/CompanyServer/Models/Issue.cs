4db59130be7f84fa79daf231c389fa7b84c1fb68
N1NTENDO1999 <denysnazarko99@gmail.com>
2019-11-30 18:11:38 +0200

    And This

--- CompanyServer/CompanyServer/Models/Issue.cs
+++ CompanyServer/CompanyServer/Models/Issue.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,6 @@ namespace CompanyServer.Models
     public class Issue
     {
         public int Id { get; set; }
-        [MaxLength(255)]
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime Created_At { get; set; }
