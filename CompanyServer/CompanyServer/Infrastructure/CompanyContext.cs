ca5bd8b2faf7c7c0683425bca579bd912497b57c
N1NTENDO1999 <denysnazarko99@gmail.com>
2019-11-30 18:30:50 +0200

    Add CompanyContext

---
+++ CompanyServer/CompanyServer/Infrastructure/CompanyContext.cs
@@ -0,0 +1,34 @@
+﻿using CompanyServer.Models;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyServer.Infrastructure
+{
+    public class CompanyContext : DbContext
+    {
+        public DbSet<Issue> Issues { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<TaskState> TaskStates { get; set; }
+        public DbSet<Team> Teams{ get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public CompanyContext(DbContextOptions<CompanyContext> options) 
+            : base(options)
+        {
+        }
+        public CompanyContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
